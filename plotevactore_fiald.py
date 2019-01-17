ee164bdc6ce01c402e112185a9502054160bbc29
MohammedAlbaqer97 <35952142+MohammedAlbaqer97@users.noreply.github.com>
2019-01-17 18:39:58 +0300

    Update plotevactore_fiald.py

--- plotevactore_fiald.py
+++ plotevactore_fiald.py
@@ -7,8 +7,8 @@ k=20
 #make mish grid
 x,y = np.meshgrid(np.linspace(-n,n,k),np.linspace(-n,n,k))
 R = np.sqrt((x)** 2 + (y) ** 2)
-# T = np.arctan2(y,x)
-a,b,m,n = 3,3,4,3
+T = np.arctan2(y,x)
+#you can chage this function and you can use T,R for polar coordinate
 u,v = -2*y,2*x
 plt.quiver(x,y,u,v,R)
 plt.show()
